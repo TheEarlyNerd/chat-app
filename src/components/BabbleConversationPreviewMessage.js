7690e5913b3761174ca150bc3b24e0c95603a17c
Braydon Batungbacal <braydon@tryshared.com>
2020-04-25 22:04:26 -0700

    More work on preview message UX

--- src/components/BabbleConversationPreviewMessage.js
+++ src/components/BabbleConversationPreviewMessage.js
@@ -1,44 +1,69 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 import { BabbleUserAvatar } from './';
-import { EyeIcon, UserIcon } from './icons';
+import { EyeIcon, UserIcon, MessageCircleIcon } from './icons';
 
 export default class BabbleConversationPreviewMessage extends Component {
   render() {
-    const { style } = this.props;
+    const { name, message, avatarUri, style } = this.props;
 
     return (
       <TouchableOpacity style={[ styles.container, style ]}>
         <BabbleUserAvatar
-          source={{ uri: 'https://www.ft.com/__origami/service/image/v2/images/raw/http%3A%2F%2Fcom.ft.imagepublish.upp-prod-us.s3.amazonaws.com%2F1e8db4c2-99c1-11e9-9573-ee5cbb98ed36?fit=scale-down&source=next&width=700' }}
+          source={{ uri: avatarUri }}
           size={50}
         />
 
         <View style={styles.content}>
           <View style={styles.spacedRow}>
-            <Text style={styles.nameText}>CryptoTalk</Text>
+            <Text style={styles.nameText}>{name}</Text>
 
-            {!this.props.conversation && (
-              <Text style={styles.timeText}>10:24 PM</Text>
-            )}
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              {this.props.conversation && (
+                <View style={styles.stats}>
+                  <View style={styles.stat}>
+                    <UserIcon width={17} height={17} style={[ styles.statIcon, styles.liveColor ]} />
+                    <Text style={[ styles.statText, styles.liveColor ]}>{Math.floor(Math.random() * 50)}</Text>
+                  </View>
 
-            {this.props.conversation && (
-              <View style={styles.stats}>
-                <View style={styles.stat}>
-                  <UserIcon width={17} height={17} style={[ styles.statIcon, styles.liveColor ]} />
-                  <Text style={[ styles.statText, styles.liveColor ]}>0</Text>
+                  <View style={styles.stat}>
+                    <EyeIcon width={17} height={17} style={styles.statIcon} />
+                    <Text style={styles.statText}>{Math.floor(Math.random() * 800) + 100}</Text>
+                  </View>
                 </View>
+              )}
 
-                <View style={styles.stat}>
-                  <EyeIcon width={17} height={17} style={styles.statIcon} />
-                  <Text style={styles.statText}>1,252</Text>
+              {this.props.unread && (
+                <View style={styles.unreadBubble}>
+                  <Text style={styles.unreadText}>{this.props.unread}</Text>
+
+                  <LinearGradient
+                    useAngle
+                    angle={36}
+                    colors={[ '#299BCB', '#1ACCB4' ]}
+                    style={styles.unreadBubbleBackground}
+                  />
                 </View>
-              </View>
-            )}
+              )}
+            </View>
           </View>
 
           <View style={styles.preview}>
-            <Text numberOfLines={(this.props.conversation) ? 3 : 1} style={styles.previewText}>Chat about your crypto investments and how we all lose money</Text>
+            <View style={{ flex: 1 }}>
+              <Text numberOfLines={3} style={[ styles.previewText, (this.props.unread) ? styles.unreadColor : null ]}>{message}</Text>
+
+              {!this.props.conversation && (
+                <Text style={styles.timeText}>10:24 PM</Text>
+              )}
+            </View>
+
+            {this.props.media && (
+              <Image
+                source={{ uri: 'https://cdn.dribbble.com/users/25514/screenshots/11009722/dribbble__5__teaser.png' }}
+                style={styles.mediaImage}
+              />
+            )}
           </View>
 
           {this.props.conversation && (
@@ -58,8 +83,21 @@ export default class BabbleConversationPreviewMessage extends Component {
                   <Text style={styles.reactionText}>🍔</Text>
                   <Text style={styles.reactionCountText}>2</Text>
                 </View>
+
+                <View style={styles.reaction}>
+                  <Text style={styles.reactionText}>🥰🥰</Text>
+                  <Text style={styles.reactionCountText}>6</Text>
+                </View>
               </View>
             </View>
+          )}
+
+          {this.props.conversation && (
+            <Text style={styles.tickerText} numberOfLines={1}>
+              <MessageCircleIcon style={styles.tickerIcon} width={14} height={14} />
+              <Text style={styles.tickerNameText}>Jack Rex: </Text>
+              yah this makes a lot of sense I guess but w/e bro
+            </Text>
           )}
         </View>
       </TouchableOpacity>
@@ -80,10 +118,11 @@ const styles = StyleSheet.create({
   spacedRow: {
     flex: 1,
     flexDirection: 'row',
+    alignItems: 'center',
     justifyContent: 'space-between',
   },
   nameText: {
-    color: '#26A6C6',
+    color: '#2A99CC',
     fontFamily: 'NunitoSans-Bold',
     fontSize: 14,
   },
@@ -91,16 +130,40 @@ const styles = StyleSheet.create({
     color: '#97A1A4',
     fontFamily: 'NunitoSans-Bold',
     fontSize: 12,
-    marginTop: 2,
+    marginTop: 4,
   },
   preview: {
-
+    flexDirection: 'row',
+    marginTop: 4,
   },
   previewText: {
+    flex: 1,
     color: '#797979',
-    fontFamily: 'NunitoSans-Bold',
+    fontFamily: 'NunitoSans-SemiBold',
     fontSize: 14,
-    marginBottom: 3,
+  },
+  unreadBubble: {
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 20,
+    height: 20,
+    marginLeft: 10,
+    borderRadius: 10,
+    shadowColor: '#252A3F',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.15,
+    shadowRadius: 2,
+  },
+  unreadBubbleBackground: {
+    ...StyleSheet.absoluteFillObject,
+    borderRadius: 10,
+    zIndex: -1,
+  },
+  unreadText: {
+    color: '#FFFFFF',
+    fontFamily: 'NunitoSans-Bold',
+    fontSize: 12,
   },
   reactions: {
     flexDirection: 'row',
@@ -114,19 +177,23 @@ const styles = StyleSheet.create({
     marginLeft: 10,
   },
   statIcon: {
-    color: '#97A1A4',
+    color: '#797979',
     marginRight: 3,
   },
   statText: {
-    color: '#97A1A4',
+    color: '#797979',
     fontFamily: 'NunitoSans-Bold',
     fontSize: 12,
   },
   liveColor: {
     color: '#FF4444',
   },
+  unreadColor: {
+    color: '#404040',
+  },
 
   reaction: {
+    marginTop: 8,
     backgroundColor: '#F1F2F6',
     flexDirection: 'row',
     alignItems: 'center',
@@ -149,5 +216,29 @@ const styles = StyleSheet.create({
     fontSize: 12,
     marginLeft: 3,
   },
+  tickerIcon: {
+    color: '#2A99CC',
+    marginBottom: -1,
+    marginRight: 5,
+  },
+  tickerNameText: {
+    color: '#2A99CC',
+  },
+  tickerText: {
+    flex: 1,
+    color: '#797979',
+    fontFamily: 'NunitoSans-SemiBoldItalic',
+    fontSize: 12,
+    marginTop: 10,
+  },
 
+
+  mediaImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 4,
+    marginTop: 4,
+    marginLeft: 10,
+    alignSelf: 'flex-start',
+  }
 });
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -17,21 +17,65 @@ export default class HomeScreen extends Component {
 
           <Text style={styles.headingText}>Direct Messages</Text>
 
-          {Array(2).fill(null).map((value, index) => (
-            <BabbleConversationPreviewMessage
-              style={styles.conversationPreviewMessage}
-              key={`prev_${index}`}
-            />
-          ))}
+          <BabbleConversationPreviewMessage
+            unread={4}
+            avatarUri={'https://scontent-sea1-1.xx.fbcdn.net/v/t1.0-1/p320x320/71572992_2728234020533981_2005024985860538368_n.jpg?_nc_cat=105&_nc_sid=dbb9e7&_nc_oc=AQkC6_7gtmxcuTEJkxUGX6b4hzh7mmoBmWcHS5WGZF8OITrVIx4nC4EqI2Q33oD8Snpzhy7EZW3GMSyv9xdQSXYF&_nc_ht=scontent-sea1-1.xx&_nc_tp=6&oh=8b49e6ea4ddca8f66a0fbc8939ec16a2&oe=5EC8C59D'}
+            name={'Braydon Batungbacal'}
+            message={'Braydon Batungbacal sent an attachment.'}
+            style={styles.conversationPreviewMessage}
+            media
+          />
 
-          <Text style={styles.headingText}>My Conversations</Text>
+          <BabbleConversationPreviewMessage
+            avatarUri={'https://scontent-sea1-1.xx.fbcdn.net/v/t1.0-1/p480x480/12717707_10205908719979367_3491263903448795284_n.jpg?_nc_cat=102&_nc_sid=dbb9e7&_nc_oc=AQkY9cvHDPkTZjpHJB2AJfWXDxK4jZexz9WgtKReqGes73lh-ujVIyMcXJRyu0LQCjbfewvRZ4lc5mRWUyJbx1n8&_nc_ht=scontent-sea1-1.xx&_nc_tp=6&oh=35573a80e7c0f39280d1dcbea302638b&oe=5ECB2090'}
+            name={'Alex Filatov'}
+            message={'There is absolutely no way that is the situation with all of this'}
+            style={styles.conversationPreviewMessage}
+          />
 
-          {Array(2).fill(null).map((value, index) => (
-            <BabbleConversationPreviewMessage
-              style={styles.conversationPreviewMessage}
-              key={`prev_${index}`}
-              conversation
-            />
+
+          <Text style={styles.headingText}>Feed</Text>
+
+          <BabbleConversationPreviewMessage
+            avatarUri={'https://static-cdn.jtvnw.net/jtv_user_pictures/ded5a0a6-7650-4df2-a0a9-0d4606c9fadf-profile_image-70x70.png'}
+            name={'ViBElol'}
+            message={'Yes, I’m stating the obvious but I will a bit strong with my speech and I’ll say what needs to be said. Protoss has not been in the leading race on Aligulac since July 2015. When was the last time a toss won a $100000+ tournament ?'}
+            style={styles.conversationPreviewMessage}
+            unread={8}
+            conversation
+            media
+          />
+
+          <BabbleConversationPreviewMessage
+            avatarUri={'https://pbs.twimg.com/profile_images/1223706175910211584/tmu8d9fA_400x400.jpg'}
+            name={'David Dobrik'}
+            message={'Hi guys!! I wanna help some more people out rather than just sending out venmos!! Reply!!'}
+            style={styles.conversationPreviewMessage}
+            conversation
+          />
+
+          <Text style={styles.headingText}>Explore</Text>
+
+          {Array(5).fill(null).map((value, index) => (
+            <>
+              <BabbleConversationPreviewMessage
+                unread={15}
+                avatarUri={'https://pbs.twimg.com/profile_images/634514155261833216/czgYrPLQ_400x400.jpg'}
+                name={'Travis Scott'}
+                message={'http://smarturl.it/ASTROWORLD'}
+                style={styles.conversationPreviewMessage}
+                conversation
+              />
+
+              <BabbleConversationPreviewMessage
+                unread={53}
+                avatarUri={'https://pbs.twimg.com/profile_images/874276197357596672/kUuht00m_400x400.jpg'}
+                name={'Donald Trump'}
+                message={'We all supporters here, #lol'}
+                style={styles.conversationPreviewMessage}
+                conversation
+              />
+            </>
           ))}
         </ScrollView>
       </View>
@@ -56,22 +100,17 @@ const styles = StyleSheet.create({
   scrollViewContainer: {
     paddingTop: 140,
     paddingHorizontal: 15,
-    paddingBottom: 100,
-  },
-  searchField: {
-    marginBottom: 40,
   },
   headingText: {
     color: '#404040',
     fontFamily: 'NunitoSans-Black',
     fontSize: 26,
-    marginBottom: 20,
+    marginTop: 40,
   },
   conversationPreviewMessage: {
-    marginBottom: 15,
-    paddingBottom: 15,
+    paddingVertical: 15,
     borderBottomWidth: 0.5,
-    borderBottomColor: '#E1E3E8',
+    borderBottomColor: '#D8D8D8',
   },
 
 });
