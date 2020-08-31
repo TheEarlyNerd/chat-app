1d683965c8747d75c850743663a6731657afac33
Braydon Batungbacal <braydon@tryshared.com>
2020-08-30 20:46:38 -0700

    Removed foreground notifications.

--- ios/Babble/AppDelegate.m
+++ ios/Babble/AppDelegate.m
@@ -97,9 +97,4 @@ didReceiveNotificationResponse:(UNNotificationResponse *)response
  [RNCPushNotificationIOS didReceiveLocalNotification:notification];
 }
 
--(void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions options))completionHandler
-{
-  completionHandler(UNAuthorizationOptionSound | UNAuthorizationOptionAlert | UNAuthorizationOptionBadge);
-}
-
 @end
