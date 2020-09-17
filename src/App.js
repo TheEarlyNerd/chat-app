99ffb5d5a2b5a849a26eb81bce0319cc33f6d148
Braydon Batungbacal <braydon@tryshared.com>
2020-09-17 02:19:05 -0700

    Integrated smartlook.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import './analytics/smartlook';
 import React, { Component } from 'react';
 import { Animated, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
---
+++ src/analytics/smartlook.js
@@ -0,0 +1,3 @@
+import Smartlook from 'smartlook-react-native-wrapper';
+
+Smartlook.setupAndStartRecording('7b0ec73fdf63d4cbe0dcae7b6234e0e4c69bc06b');
